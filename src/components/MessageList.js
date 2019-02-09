706c3c15d2e47a5252496b4995de42de60bdf775
Guanhua <william.ting.1116@gmail.com>
2019-02-09 23:18:27 +1000

    edited start code

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
 
-class MessageList extends Component {
-    render() {
-        return (
-            <div className="message-list">
-                <div className="help-text">MessageList</div>
-            </div>
-        )
+import Message from './Message'
+
+const DUMMY_DATA = [
+    {
+        senderId: 'perborgen',
+        text: 'Hey, how is it going?'
+    },
+    {
+        senderId: 'janedoe',
+        text: 'Great! How about you?'
+    },
+    {
+        senderId: 'perborgen',
+        text: 'Good to hear! I am great as well'
+    },
+    {
+        senderId: 'perborgen',
+        text: 'Good to hear! I am great as well'
     }
+]
+
+class MessageList extends Component {
+  constructor(){
+    super()
+    this.state = { messages : DUMMY_DATA }
+  }
+
+  render() {
+      return (
+          <div className="message-list">
+
+            <Message />
+          </div>
+      )
+  }
 }
 
 export default MessageList;
